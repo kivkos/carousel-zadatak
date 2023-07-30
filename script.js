a0e53a1314df5dbf6b746cc543a1e23b9f455121
kivkos <127855349+kivkos@users.noreply.github.com>
2023-07-30 13:35:14 +0200

    ispravak

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-$(document).ready(function() {
-    $('.next-btn').click(function() {
-      var carouselRow = $('.carousel-row');
-      var rowWidth = carouselRow.width();
-      var images = carouselRow.find('.carousel-slika');
-  
-      var lastVisibleImage;
-      var totalWidth = 0;
-  
-      images.each(function() {
-        totalWidth += $(this).outerWidth();
-        if (totalWidth <= rowWidth) {
-          lastVisibleImage = $(this);
-        }
-      });
-  
-      var slide = rowWidth - lastVisibleImage.outerWidth();
-  
-      carouselRow.css('transform', 'translateX(-' + slide + 'px)');
-    });
-  
-    $('.prev-btn').click(function() {
-      var carouselRow = $('.carousel-row');
-      var rowWidth = carouselRow.width();
-      var images = carouselRow.find('.carousel-slika');
-  
-      var firstVisibleImage;
-      var totalWidth = 0;
-  
-      images.each(function() {
-        totalWidth += $(this).outerWidth();
-        if (totalWidth <= rowWidth) {
-          firstVisibleImage = $(this);
-        }
-      });
-  
-      var slide = images.first().position().left;
-  
-      carouselRow.css('transform', 'translateX(' + (-slide) + 'px)');
-    });
-  });
+// Pomicanje lijevo
+function moveCarouselLeft() {
+  const rowFirst = document.getElementById('row-first');
+  const rowSecond = document.getElementById('row-second');
+
+  rowFirst.style.transition = 'transform 0.5s ease';
+  rowSecond.style.transition = 'transform 0.5s ease';
+
+  rowFirst.insertBefore(rowFirst.lastElementChild, rowFirst.firstElementChild);
+
+  // dodavanje prve slike na kraj reda
+  rowSecond.appendChild(rowSecond.firstElementChild.cloneNode(true));
+  rowSecond.removeChild(rowSecond.firstElementChild);
+}
+
+// pomicanje desno
+function moveCarouselRight() {
+  const rowFirst = document.getElementById('row-first');
+  const rowSecond = document.getElementById('row-second');
+
+  rowFirst.style.transition = 'transform 0.5s ease';
+  rowSecond.style.transition = 'transform 0.5s ease';
+
+  rowFirst.appendChild(rowFirst.firstElementChild.cloneNode(true));
+  rowFirst.removeChild(rowFirst.firstElementChild);
+
+  // dodavanje zadnje slike na početak
+  rowSecond.insertBefore(rowSecond.lastElementChild, rowSecond.firstElementChild);
+}
+
+document.querySelector('.prev-btn').addEventListener('click', moveCarouselRight);
+document.querySelector('.next-btn').addEventListener('click', moveCarouselLeft);
